e5f83d21f848364840ff3493ff18fdf190ddeba3
divyanshujain11 <77712311+divyanshujain11@users.noreply.github.com>
2023-08-07 13:47:51 +0530

    Create send_mail.py

---
+++ send_mail.py
@@ -0,0 +1,7 @@
+#send email using pyhwhatkit
+import pywhatkit as p
+sender_mail = "albelaraja23@gmail.com"
+subject = "this is test mail:\n"
+message = "hello biro:\n"
+receiver_mail = "jaind3010@gmail.com: "
+p.send_mail(sender_mail, 'vhfcvgxtvzelzxqs', subject, message, receiver_mail)
